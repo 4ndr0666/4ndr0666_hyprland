d74679d8c61fa9416e2a7f3e495846bb72002cb6
4ndr0666 <01_dolor.loftier@icloud.com>
2026-08-25 17:22:00 -0500

    refactor: replace removed global UI dependency

---
+++ install-scripts/core/ui.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Minimal installer UI primitives shared by non-package modules.
+
+RESET=$'\033[0m'
+SKY_BLUE=$'\033[96m'
+YELLOW=$'\033[33m'
+MAGENTA=$'\033[35m'
+NOTE='[INFO]'
+INFO='[INFO]'
+OK='[OK]'
+ERROR='[ERROR]'
