7c35e6ae86e0b396ab1b58eba710b739bb2b5446
4ndr0666 <01_dolor.loftier@icloud.com>
2026-08-29 02:40:47 -0500

    normalize gesture settings after Lua zoom migration

--- config/hypr/configs/SystemSettings.lua
+++ config/hypr/configs/SystemSettings.lua
@@ -57,8 +57,6 @@ hl.config({
         workspace_swipe_min_speed_to_force = 30,
         workspace_swipe_cancel_ratio = 0.5,
         workspace_swipe_create_new = true,
-        workspace_swipe_distance = 500,
-        workspace_swipe_create_new = true,
         workspace_swipe_forever = true,
     },
     misc = {
