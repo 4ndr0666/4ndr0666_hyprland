8d07ed5cf94f15024f5a579da73d0daa1d2d4448
4ndr0666 <01_dolor.loftier@icloud.com>
2026-09-09 22:07:23 -0500

    test(gup): include installer dry-run contract

--- tests/unit/run-golden-units.sh
+++ tests/unit/run-golden-units.sh
@@ -76,6 +76,7 @@ TESTS=(
   tests/unit/test-waybar-link-transaction.sh
   tests/unit/test-clock-12h-mandatory.sh
   tests/unit/test-gtk-theme-installer.sh
+  tests/unit/test-installer-dry-run.sh
 )
 
 failures=()
